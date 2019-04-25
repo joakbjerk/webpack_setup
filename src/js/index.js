76b614460cc44d7f9f211a9ca008179dc51f595c
joakbjerk <joakbjerk@gmail.com>
2019-04-25 18:54:41 +0200

    Master conflict

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { Component } from './component';
 import '../styles/styles.scss';
 
+//The unconflict
 render(
     <Component />,
     document.querySelector("#render-target")
